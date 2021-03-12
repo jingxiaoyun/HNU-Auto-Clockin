a22cf261e40f57d8b5e419969d9e9ef7765c4553
jingxiaoyun <66993953+jingxiaoyun@users.noreply.github.com>
2021-03-12 22:39:10 +0800

    Update clock_in.py

--- clock_in.py
+++ clock_in.py
@@ -31,12 +31,13 @@ def login():
     token, captcha = captchaOCR()
     login_info = {"Code":args.username,"Password":args.password,"VerCode":captcha,"Token":token}
     
-    set_cookie = requests.post(login_url, json=login_info).headers['set-cookie']
-    regex = r"\.ASPXAUTH=(.*?);"
-    ASPXAUTH = re.findall(regex, set_cookie)[2]
+    set_cookie = requests.post(login_url, json=login_info).headers
+    print(set_cookie)
+#     regex = r"\.ASPXAUTH=(.*?);"
+#     ASPXAUTH = re.findall(regex, set_cookie)[2]
 
-    headers = {'Cookie': f'.ASPXAUTH={ASPXAUTH}; TOKEN={token}'}
-    return headers
+#     headers = {'Cookie': f'.ASPXAUTH={ASPXAUTH}; TOKEN={token}'}
+#     return headers
 
 def setLocation():
     location = json.loads(requests.get(f'http://api.tianditu.gov.cn/geocoder?ds={{"keyWord":\"{args.province+args.city+args.county}\"}}&tk=2355cd686a32d016021bffbc4a69d880').text)["location"]
@@ -46,40 +47,40 @@ def setLocation():
 def main():
     clockin_url = 'https://fangkong.hnu.edu.cn/api/v1/clockinlog/add'
     headers = login()
-    lon, lat, real_address = setLocation()
-    clockin_data = {"Temperature":"null",
-                    "RealProvince":args.province,
-                    "RealCity":args.city,
-                    "RealCounty":args.county,
-                    "RealAddress":real_address,
-                    "IsUnusual":"0",
-                    "UnusualInfo":"",
-                    "IsTouch":"0",
-                    "IsInsulated":"0",
-                    "IsSuspected":"0",
-                    "IsDiagnosis":"0",
-                    "tripinfolist":[{"aTripDate":"","FromAdr":"","ToAdr":"","Number":"","trippersoninfolist":[]}],
-                    "toucherinfolist":[],
-                    "dailyinfo":{"IsVia":"0","DateTrip":""},
-                    "IsInCampus":"1",
-                    "IsViaHuBei":"0",
-                    "IsViaWuHan":"0",
-                    "InsulatedAddress":"",
-                    "TouchInfo":"",
-                    "IsNormalTemperature":"1",
-                    "Longitude":lon,
-                    "Latitude":lat
-                    }
+#     lon, lat, real_address = setLocation()
+#     clockin_data = {"Temperature":"null",
+#                     "RealProvince":args.province,
+#                     "RealCity":args.city,
+#                     "RealCounty":args.county,
+#                     "RealAddress":real_address,
+#                     "IsUnusual":"0",
+#                     "UnusualInfo":"",
+#                     "IsTouch":"0",
+#                     "IsInsulated":"0",
+#                     "IsSuspected":"0",
+#                     "IsDiagnosis":"0",
+#                     "tripinfolist":[{"aTripDate":"","FromAdr":"","ToAdr":"","Number":"","trippersoninfolist":[]}],
+#                     "toucherinfolist":[],
+#                     "dailyinfo":{"IsVia":"0","DateTrip":""},
+#                     "IsInCampus":"1",
+#                     "IsViaHuBei":"0",
+#                     "IsViaWuHan":"0",
+#                     "InsulatedAddress":"",
+#                     "TouchInfo":"",
+#                     "IsNormalTemperature":"1",
+#                     "Longitude":lon,
+#                     "Latitude":lat
+#                     }
 
-    clockin = requests.post(clockin_url, headers=headers, json=clockin_data)
+#     clockin = requests.post(clockin_url, headers=headers, json=clockin_data)
 
-    if clockin.status_code == 200:
-        if '成功' in clockin.text or '已提交' in clockin.text:
-            isSucccess = 0
-    else:
-        isSucccess = 1
-    print(json.loads(clockin.text)['msg'])
+#     if clockin.status_code == 200:
+#         if '成功' in clockin.text or '已提交' in clockin.text:
+#             isSucccess = 0
+#     else:
+#         isSucccess = 1
+#     print(json.loads(clockin.text)['msg'])
 
-    return isSucccess
+#     return isSucccess
 
 main()
